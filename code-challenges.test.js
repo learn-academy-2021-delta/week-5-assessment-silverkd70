afcf9040e8769ec625f8abeaacb8258b5e470cce
silverkd70 <silverkd70@icloud.com>
2021-10-11 17:05:47 -0700

    assessment completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,7 +14,27 @@
 
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
+//pseudo code
+
+// declare a constant variable
+// use the string perameter in the function action
+// set a new variable and use built-in method replace with regular expressions to loop through the string with global and ignore 
+// set replace loop to convert string letter to required number
+// return the new variable to return the coded message output
+
 // a) Create a test with expect statements using the variables provided.
+
+describe('textCoder', () => {
+    test("takes in a string and converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0 and returns a coded string", () => {
+        expect(textCoder("Lackadaisical")).toEqual("L4ck4d41s1c4l") 
+        expect(textCoder("Gobbledygook")).toEqual("G0bbl3dyg00k") 
+        expect(textCoder("Eccentric")).toEqual("3cc3ntr1c")
+    })
+})
+
+//test failed
+    //ReferenceError: textCoder is not defined
+//rewriting code to define textCoder, then running second test
 
 var secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
@@ -27,11 +47,35 @@ var secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+const textCoder = (str) => {
+    let stringCode = str.replace(/a/gi, '4').replace(/e/gi, '3').replace(/i/gi, '1').replace(/o/gi, '0');
+      return stringCode;
+  }
+
 
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
+//pseudo code
+
+// declare a constant variable
+// use the array and string perameter in the function action
+// set a new variable to filter the array 
+// call on the value function to return the value to iterate through the array and set to lowercase and use the built-in method to include the string
+// return the new variable array to output only the words in the array that are associated with the variable letter assigned.
+
 // a) Create a test with expects statement using the variable provided.
+
+describe('wordFinder', () => {
+    test("takes in an array of words and a single letter string and returns a new array containing only the words that include the single letter string defined", () => {
+        expect(wordFinder(["Apple", "Banana", "Plum", "Orange", "Kiwi"], "a")).toEqual(["Apple", "Banana", "Orange"]) 
+        expect(wordFinder(["Mango", "Cherry", "Apricot", "Blueberry", "Peach"], "e")).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
+
+//test failed
+    //ReferenceError: wordFinder is not defined
+//rewriting code to define wordFinder, then running second test
 
 var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
 var letterA = "a"
@@ -44,11 +88,40 @@ var letterE = "e"
 
 // b) Create the function that makes the test pass.
 
+const wordFinder = (arr, str) => {
+    let newArr = arr.filter(words => {
+      return words.toLowerCase().includes(str)
+      
+  })
+  return newArr;
+  }
+
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
+//pseudo code
+
+// declare a constant variable
+// use the array perameter in the function action
+// declare a new variable to create an array with two indexes set to 0 in order to set up a number count verification
+// use conditionals and two for loops to iterate through two separate indexes to count and identify duplicate numbers
+// set if and else if conditionals to determine if the array of numbers = truthy or falsey for a "full house" of exactly one pair and one three of a kind.
+// create returns on each if and else if conditionals
+
 // a) Create a test with expect statements using the variable provided.
+
+describe('handChecker', () => {
+    test('takes in an array of 5 numbers and iterates through each number to identify if the array has 3 like numbers + 2 other like numbers for a truthy or else the array of numbers is falsey.', () => {
+        expect(handChecker([5, 5, 5, 3, 3])).toBe(true) 
+        expect(handChecker([5, 5, 3, 3, 4])).toBe(false)
+        expect(handChecker([5, 5, 5, 5, 4])).toBe(false)
+    })
+})
+
+//test failed
+    //ReferenceError: handChecker is not defined
+//rewriting code to define handChecker, then running second test
 
 var hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -60,3 +133,30 @@ var hand3 = [5, 5, 5, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+const handChecker = (arr) => {
+    let handCount = [0,0]
+    //let numChecker = 0  //not sure why I created this because it is not necessary for this to work. 
+    for (i=0; i<arr.length; i++){
+        if (arr[0] === arr[i]){
+            handCount[0]++
+        }
+        else {
+            numChecker = arr[i]
+        }
+      }
+    for (i=0; i<arr.length; i++){
+        if (arr[i] === numChecker){
+              handCount[1]++
+            }
+    } if ((handCount[0] !== 2) && (handCount[0] !== 3)){
+        return false
+    } else if (arr[i] === numChecker){
+            handCount[1]++
+        
+    } else if ((handCount[1] !==2) && (handCount[1] !==3)){
+        return false
+    } else {
+      return true
+  }
+  }
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Are (most) Ruby methods accessors or mutators?
 
-  Your answer:
+  Your answer: Ruby methods are accessors because they are called up rather than used as mutators to change things.  We call on the objects by using getter and setter methods.
 
-  Researched answer:
+  Researched answer: Ruby methods don't change the object they are called on and the accessors are ways in creating the getter and setter methods to help assign and retrieve values. Attribute accessors are also used to create other methods.  
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: A block is similar to writing a function.  We can use a block with keywords do and end to call on the function and ensure that the function stops by adding the end call.
 
-  Researched answer:
+  Researched answer: After more research a Ruby block has anonymous functions and are codes passed in a method. It is a way of grouping statements. A block is started on the same line as the method call and it is not executed at the time it is encountered.  
 
 
 
 3. What is a gem?
 
-  Your answer:
+  Your answer: I believe that a gem is like a package manager or library with some code and functionality.  We used gem to load rspec for our Ruby testing.  I think I remember that gem is built in Mac OS and allows us to type the gem command to also preload other package managers, as well.
 
-  Researched answer:
+  Researched answer: It seems that gem packages up Ruby code and is a library.  The gem command is used to build, upload, and istall the gem packages.  It is an open source library and allows programmers to access code within gem and can be used to extend or modify Ruby functionality.
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: A relational database is similar to an excel spreadsheet with columns and rows with data that is based on having relationships with each other.  It stores information in tables and allows for better structure and easier quary calls on objects or methods to pull the data a programmer is looking for or to use. Other kinds of databases would be SQL and Postgres.
 
-  Researched answer:
+  Researched answer: Ralational databases help the programmer to better organize their data and allows users to manage predefined data relationships.  Relational databases are similar to key:value pairs and can be used by multiple programming tools.
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: Unique identifiers for the database rows in the table that prevents duplicate instances.  Primary keys are identified by their own row 'ID'.
 
-  Researched answer:
+  Researched answer: Found out that the primary key is a special relational database table column that is designated to uniquely identify each table record and cannot contain null values.  It is used to quickly resolve data within the table.  The primary key in a table column is important because it serves the purpose because it ensures row-level accessibility.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: Rails follows a RESTful pattern for interaction between the client and the server. RESTful routes are attempts to bring the operations that can be applied to an object in HTTP requests.  Also, it is a set of standards that allows it to be accessed by many different languages to create efficient and reusable routes, aiming to map HTTP methods and CRUD actions.
 
-2. JSON:
+2. JSON: (JavaScript Object Notation) is a lightweight data-interchange format.  It is easy to read and write and easy for machines to resolve data and generate data.  It is based on a subset of the JavaScript Programming Language and is built on  a collection of name/value pairs and is an ordered list of values.
 
-3. ERB:
+3. ERB: it is an embedded Ruby extenstion and is a file structure that allows us to code in HTML and drop Ruby code into the view file. It is a feature of Ruby that enables you to conveniently generate any kind of text, in any quantity, from templates.
 
-4. Params:
+4. Params: Stands for Parameters and allows us to add more information to our program by passing a value into the controller. It is a hash and is the collection of data received by the application during a particular HTTP request.  
 
-5. API:
+5. API: (Application Programming Interface) help to simplify software development and innovation by enabling applications to exchange data and functionality easily and securely. They are necessary in order to bring applications together in order to perform a designed function built around sharing data and executing pre-defined processes. It is a set of code that facilitates the way software interacts with other software and is the communication between the software that facilitates interaction.
